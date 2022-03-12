529054edc582bb183ba87e4e0221a19b4e9e2b7f
AdaSeteye <you@example.com>
2022-03-12 02:51:59 -0800

    harder

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Description: Get a random number and print it
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
