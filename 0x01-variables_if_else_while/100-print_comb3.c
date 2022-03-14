8bce1e6b80f73743737d5da67039c0c9b2b51564
AdaSeteye <you@example.com>
2022-03-13 17:06:37 -0700

    matured

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - main block
+ *Description: it computes what is required
+ *Return: 0
+ */
+int main() 
+{
+	int i,j;
+
+	for (i=0; i<10; i++)
+	{
+		for (j=i+1; j<10; j++)
+	        {
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}				       
+	return 0;
+}
