af25876b6856a55197aef1568c431dd7e293a91d
AdaSeteye <you@example.com>
2022-03-13 18:20:13 -0700

    got

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,19 +4,21 @@
  *Description: it computes what is required
  *Return: 0
  */
-int main() 
+int main(void) 
 {
-	int i,j;
-
-	for (i=0; i<10; i++)
+	int i, j;
+	for (i = 0; i < 10; i++)
 	{
-		for (j=i+1; j<10; j++)
+		for (j = i + 1; j < 10; j++)
 	        {
 			putchar(i + '0');
 			putchar(j + '0');
-			putchar(',');
-			putchar(' ');
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}				       
-	return 0;
+	return (0);
 }
